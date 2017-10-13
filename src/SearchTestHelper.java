b9c3092463d025235d0a8563dec7fd607ecd6545
Yeming <492851378@qq.com>
2017-10-13 23:00:05 +0800

    实现了SearchTestHelper

--- src/SearchTestHelper.java
+++ src/SearchTestHelper.java
@@ -1,6 +1,14 @@
 //这个类用来生成，测试所需的随机数
 public class SearchTestHelper {
     public static Integer[] generateRandomArray(int n, int rangeL, int rangeR){
-        return null;
+        assert rangeL <= rangeR;
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Integer((int)(Math.random()*(rangeR+1-rangeL)+rangeL));
+        }
+
+        return arr;
     }
 }
