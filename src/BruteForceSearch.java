79ab0e7b9511afc7f4b0b5e6ab88001d12fc437f
Yeming <492851378@qq.com>
2017-10-13 22:52:30 +0800

    实现了BruteForceSearch

--- src/BruteForceSearch.java
+++ src/BruteForceSearch.java
@@ -1,3 +1,9 @@
 // 暴力查找
 public class BruteForceSearch {
+    public static Comparable rank(Comparable key, Comparable[] a) {
+        for (Comparable ele : a) {
+            if(ele==key) return ele;
+        }
+        return -1;
+    }
 }
