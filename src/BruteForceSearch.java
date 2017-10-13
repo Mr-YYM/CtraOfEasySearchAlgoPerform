0abfbf7e94b51fd984bdc1d3835481630b3e84f6
Yeming <492851378@qq.com>
2017-10-14 00:05:40 +0800

    写了用例，有个奇怪的bug...

--- src/BruteForceSearch.java
+++ src/BruteForceSearch.java
@@ -1,9 +1,13 @@
 // 暴力查找
 public class BruteForceSearch {
     public static Comparable rank(Comparable key, Comparable[] a) {
-        for (Comparable ele : a) {
-            if(ele==key) return ele;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]==key) return a[i];
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rank(2, new Integer[]{1, 2, 3}));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 10000;
+        Integer[] arr = SearchTestHelper.generateRandomArray(n, 1, 30000 );
+        Arrays.sort(arr);
 
+//        BinarySearch part
+        int key = new Scanner(System.in).nextInt();
+        long start = System.currentTimeMillis();
+        key = (int) BinarySearch.rank(key,arr);
+        long end = System.currentTimeMillis();
+        System.out.printf("%d ms @%d\n",(end-start),key);
+
+        //BruteForceSearch part
+        start = System.currentTimeMillis();
+        key = (int) BruteForceSearch.rank(key,arr);
+        end = System.currentTimeMillis();
+        System.out.printf("%d ms @%d",(end-start),key);
     }
 }
