d7ed4756ae1bcaad8f649f7a4442c1a6746ec0ef
Yeming <492851378@qq.com>
2017-10-13 09:57:09 +0800

    实现了BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,3 +1,14 @@
 //二分查找
 public class BinarySearch {
+    public static Comparable rank(Comparable key, Comparable[] a){
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if      (key.compareTo(a[mid])<0) hi = mid - 1;
+            else if (key.compareTo(a[mid])>0) lo = mid + 1;
+            else                            return mid;
+        }
+        return -1;
+    }
 }
