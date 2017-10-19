998c700e53bf00fecfc263e53fb1bbdf7b4d6a46
Yeming <492851378@qq.com>
2017-10-19 21:47:28 +0800

    根据阿里提出的规范，给If语句块加上大括号

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -5,9 +5,13 @@ public class BinarySearch {
         int hi = a.length - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if      (key.compareTo(a[mid])<0) hi = mid - 1;
-            else if (key.compareTo(a[mid])>0) lo = mid + 1;
-            else                            return a[mid];
+            if        (key.compareTo(a[mid])<0) {
+                hi = mid - 1;
+            } else if (key.compareTo(a[mid])>0) {
+                lo = mid + 1;
+            } else {
+                return a[mid];
+            }
         }
         return -1;
     }
--- src/BruteForceSearch.java
+++ src/BruteForceSearch.java
@@ -2,7 +2,9 @@
 public class BruteForceSearch {
     public static Comparable rank(Comparable key, Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
-            if(a[i]==key) return a[i];
+            if(a[i]==key) {
+                return a[i];
+            }
         }
         return -1;
     }
